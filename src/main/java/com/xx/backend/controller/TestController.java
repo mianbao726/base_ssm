bfa0abb5ecca9fadf87825bd88985e80ed58a614
miabao726 <mianbao726@163.com>
2017-04-06 14:44:17 +0800

    Signed-off-by: miabao726 <mianbao726@163.com>

---
+++ src/main/java/com/xx/backend/controller/TestController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2017, 2017, WHATGOOGLE and/or its affiliates. All rights reserved.
+ * WHATGOOGLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+package com.xx.backend.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xx.base.controller.BaseController;
+
+/**
+ * @author generate by www.whatgoogle.com (ps : some question? contact zhuwj@726@gmail.com)
+ */
+@Controller
+@RequestMapping("/Test")
+public class TestController extends BaseController {
+
+	/**
+	 * @author generate by www.whatgoogle.com (ps : some question? contact zhuwj@726@gmail.com)
+	 */
+	 @RequestMapping("/dashboard.html")
+	public String getnewone(HttpServletRequest request, Model model)throws Exception{
+		return "dashboard/index";
+	}
+}
