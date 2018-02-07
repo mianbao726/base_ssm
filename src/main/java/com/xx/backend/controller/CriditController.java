485cee52aeda6c8439dd4c9746993b57ee5be107
mianbao726 <mianbao726@163.com>
2018-02-07 17:24:35 +0800

    Signed-off-by: mianbao726 <mianbao726@163.com>

--- src/main/java/com/xx/backend/controller/CriditController.java
+++ src/main/java/com/xx/backend/controller/CriditController.java
@@ -44,6 +44,10 @@ import com.xx.base.controller.BaseController;
 import com.xx.base.util.map.QMap;
 
 /**
+ * spend 花销  
+ * repay 还款
+ * check 检查
+ * water 线下流水 
  * @author generate by www.whatgoogle.com (ps : some question? contact
  *         zhuwj726@gmail.com)
  */
